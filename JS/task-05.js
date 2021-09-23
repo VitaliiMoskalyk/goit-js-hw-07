a8f23768b06d0d6e5ed320a004f161dd30f0091f
VitaliiMoskalyk <vitalikmoskalik@gmail.com>
2021-09-23 20:59:01 +0300

    +

--- JS/task-05.js
+++ JS/task-05.js
@@ -6,6 +6,7 @@
 const inputRef = document.querySelector('#name-input');
 const outputRef = document.querySelector('#name-output');
 
+inputRef.addEventListener('input',changeOutputName);
 
 function changeOutputName(event) {
     if (event.currentTarget.value) outputRef.textContent=event.currentTarget.value;
@@ -13,4 +14,3 @@ function changeOutputName(event) {
 };
 
 
-inputRef.addEventListener('input',changeOutputName)
