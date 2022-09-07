cc0a8834b5e2a2734df340efe3e02ed06fdf9e09
VitaliiMoskalyk <vitalikmoskalik@gmail.com>
2022-09-07 23:52:58 +0300

    ts-test

--- JS/task-03.js
+++ JS/task-03.js
@@ -10,35 +10,35 @@
 // Добавь минимальное оформление галереи флексбоксами или гридами
 // через css - классы.
 
-
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
 
+const galleryRef = document.querySelector("#gallery");
 
-const galleryRef = document.querySelector('#gallery');
-
-
-const createLiTags = (array) => {
-    const liArray = [];
-    array.map(element => {
-        liArray.push(`<li><img src='${element.url}' width='720' alt='${element.alt}'></li>`);
-        return liArray;
-    })
-    return liArray.join('');
+const createLiTags = function (array) {
+  const liArray = [];
+  array.map(function (element) {
+    liArray.push(
+      "<li><img src='"
+        .concat(element.url, "' width='720' alt='")
+        .concat(element.alt, "'></li>")
+    );
+    return liArray;
+  });
+  return liArray.join("");
 };
 
-galleryRef.insertAdjacentHTML('afterbegin', createLiTags(images));
-
-//console.log(createLiTags(images));
+galleryRef !== null &&
+  galleryRef.insertAdjacentHTML("afterbegin", createLiTags(images));
